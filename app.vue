a753fe708cef05491dbab01047b4c61dd08f2dd7
zhuchentong <2037630@163.com>
2023-08-16 11:56:49 +0800

    update app.vue

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <NaiveConfig :theme-config="themeConfig">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </NaiveConfig>
 </template>
+
+<script setup lang="ts">
+import type { ThemeConfig } from '@bg-dev/nuxt-naiveui'
+
+const themeConfig: ThemeConfig = {
+  shared: {}, // Common theme options
+  mobileOrTablet: {}, // Theme options applied on mobile and tablet
+  mobile: {}, // Theme options applied on mobile only
+  light: {}, // Theme options applied on light mode
+  dark: {}, // Theme options applied on dark mode
+}
+</script>
